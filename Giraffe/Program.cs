059a1d26e0958c7a8e6e4478c843f26b6d66293b
Carl Kakisis <Messinias1@gmail.com>
2020-01-30 01:48:51 -0500

    creating constructors

--- Giraffe/Program.cs
+++ Giraffe/Program.cs
@@ -10,15 +10,17 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            Book book1 = new Book();
-            book1.title = "Harry Potter";
-            book1.author = "JK Rowling";
-            book1.pages = 400;
+            Book book1 = new Book("Harry Potter", "JK Rowling", 400);
+            Book book2 = new Book("The Lord Of The Rings", "JRR Tolkien", 700);
+            book2.title = "The Hobbit";
 
-            Book book2 = new Book();
-            book2.title = "The Lord Of The Rings";
-            book2.author = "JRR Tolkien";
-            book2.pages = 700;
+            Book book3 = new Book();
+            book3.title = "Heavier Than Heaven";
+            book3.author = "Charles R Cross";
+            book3.pages = 393;
+
+            Console.WriteLine(book2.title);
+            Console.WriteLine(book3.pages);
         }
     }
 }
